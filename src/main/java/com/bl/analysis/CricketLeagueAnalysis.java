03fa54fa9e25f0d535072d04e5987b75b024cacf
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-08 13:47:21 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/bl/analysis/CricketLeagueAnalysis.java
@@ -0,0 +1,7 @@
+package com.bl.analysis;
+public class CricketLeagueAnalysis {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Cricket League Analysis");
+    }
+}
