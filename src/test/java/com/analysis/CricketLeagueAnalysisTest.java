8b8635b82a63f1ee1e17c3de623ba568dd67a9c9
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-08 17:56:14 +0530

    ADDED:Checked Number Of Record Matches Of Csv File

--- src/test/java/com/analysis/CricketLeagueAnalysisTest.java
+++ src/test/java/com/analysis/CricketLeagueAnalysisTest.java
@@ -2,11 +2,21 @@ package com.analysis;
 import com.bl.analysis.model.CricketLeagueAnalysis;
 import org.junit.Assert;
 import org.junit.Test;
-public class CricketLeagueAnalysisTest
-{
+import static com.bl.analysis.model.FileUtility.IPL_RUN_SHEET;
+
+public class CricketLeagueAnalysisTest {
     CricketLeagueAnalysis cricketLeagueAnalysis;
 
     public void setUP() {
         cricketLeagueAnalysis = new CricketLeagueAnalysis();
+    }
+
+    @Test
+    public void givenCsvFile_whenNumberOfRecordMatches_thenReturnTrue() {
+        try {
+            int numberOdRecords = cricketLeagueAnalysis.LoadIPLData(IPL_RUN_SHEET);
+            Assert.assertEquals(100, numberOdRecords);
+        } catch (Exception e) {
+        }
     }
 }
